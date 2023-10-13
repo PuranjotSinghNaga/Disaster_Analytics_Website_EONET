46447ae9c532b339adda8b6c22a8920b1d1a86ef
Puranjot Singh <puranjotsinghnaga@gmail.com>
2023-10-13 11:53:30 +0530

    dynamic select options added & current disaster

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const currentDisaster = await axios.get(
+      "https://eonet.gsfc.nasa.gov/api/v3/events?limit=5&days=20&source=InciWeb&status=open"
+    );
+    res.render("index.ejs", {
+      cuurent: currentDisaster,
+    });
+  } catch (error) {
+    console.error("Failed to make request", error.message);
+    res.status(500).send("Falied to fetch activity");
+  }
 });
 
 app.get("/search", async (req, res) => {
   try {
-    const formValue = req.query.selectValue;
-    const response = await axios.get(
-      `https://eonet.gsfc.nasa.gov/api/v3/events?${formValue}`
-    );
-    console.log(formValue);
+    var formValue1 = req.query.selectValue1;
+    var formValue2 = req.query.selectValue2;
+    // const response = await axios.get(
+    //   `https://eonet.gsfc.nasa.gov/api/v3/events?${formValue1}=`
+    // );
+    console.log(formValue1);
+    console.log(formValue2.toLowerCase());
 
     res.redirect("/");
   } catch (error) {
--- public/scripts.js
+++ public/scripts.js
@@ -18,15 +18,55 @@ var types = {
 };
 
 var main = document.getElementById("mainMenu");
-var sub = document.getElementById("subMenu");
+var subContainer = document.getElementById("subMenuContainer");
 
 main.addEventListener("change", function () {
-  var selected_option = types[this.value];
-  while (sub.options.length > 0) {
-    sub.options.remove(0);
+  var selected_option = this.value;
+  var sub = document.getElementById("subMenu");
+
+  if (selected_option === "location") {
+    // Create a text input field for location
+    var locationInput = document.createElement("input");
+    locationInput.type = "text";
+    locationInput.className = "form-control";
+    locationInput.name = "selectValue2";
+    locationInput.placeholder = "Enter Location";
+    // Replace the subMenu with the locationInput
+    subContainer.innerHTML = "";
+    subContainer.appendChild(locationInput);
+  } else if (selected_option === "date") {
+    locationInput = document.createElement("input");
+    locationInput.type = "date";
+    locationInput.name = "selectValue2";
+    locationInput.className = "datePicker";
+    subContainer.innerHTML = "";
+    subContainer.appendChild(locationInput);
+  } else if (selected_option === "days") {
+    locationInput = document.createElement("input");
+    locationInput.type = "number";
+    locationInput.name = "selectValue2";
+    locationInput.className = "daysInput";
+    locationInput.min = "0";
+    subContainer.innerHTML = "";
+    subContainer.appendChild(locationInput);
+  } else {
+    var selected_values = types[selected_option];
+    subContainer.innerHTML = "";
+
+    // Create and populate the subMenu with options
+    var select = document.createElement("select");
+    select.className = "form-select";
+    select.ariaLabel = "Default select example";
+    select.name = "selectValue2";
+
+    var defaultOption = new Option("Search By:", "");
+    select.appendChild(defaultOption);
+
+    selected_values.forEach(function (el) {
+      let option = new Option(el, el);
+      select.appendChild(option);
+    });
+
+    subContainer.appendChild(select);
   }
-  Array.from(selected_option).forEach(function (el) {
-    let option = new Option(el, el);
-    sub.appendChild(option);
-  });
 });
