5b5e714f07e8efa7abfe9e2fafce8d6fc07b737c
Puranjot Singh <puranjotsinghnaga@gmail.com>
2023-10-20 10:24:00 +0530

    changed listing of elements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
@@ -25,14 +25,24 @@ app.get("/", async (req, res) => {
 app.get("/search", async (req, res) => {
   try {
     var formValue1 = req.query.selectValue1;
-    var formValue2 = req.query.selectValue2;
-    // const response = await axios.get(
-    //   `https://eonet.gsfc.nasa.gov/api/v3/events?${formValue1}=`
-    // );
+    var formValue2 = req.query.selectValue2.replace(/\s/g, "").toLowerCase();
+    const currentDisaster = await axios.get(
+      "https://eonet.gsfc.nasa.gov/api/v3/events?days=20&status=open"
+    );
+    console.log(
+      `https://eonet.gsfc.nasa.gov/api/v3/events?${formValue1}=${formValue2}`
+    );
+    const response = await axios.get(
+      `https://eonet.gsfc.nasa.gov/api/v3/events?${formValue1}=${formValue2}`
+    );
+
     console.log(formValue1);
     console.log(formValue2.toLowerCase());
 
-    res.redirect("/");
+    res.render("index.ejs", {
+      current: currentDisaster,
+      response: response,
+    });
   } catch (error) {
     console.error("Failed to make request", error.message);
     res.status(500).send("Falied to fetch activity");
